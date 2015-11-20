976cb2de6a363108688a0eebadea03398b717474
Guillaume Soullard <gsoullard@interactiv-technologies.com>
2015-11-20 18:58:58 +0100

    menage dans le projet

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: guillaumesoullard
- * Date: 09/09/15
- * Time: 22:36
+ * Date: 20/11/15
+ * Time: 18:52
  */
 
 namespace AppBundle\Controller;
@@ -11,11 +11,11 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class ConnectionController extends Controller
+class HomeController extends Controller
 {
     public function indexAction()
     {
-        $html = $this->container->get('templating')->render('connection/index.html.twig');
+        $html = $this->container->get('templating')->render('home/index.html.twig');
         return new Response($html);
 
     }
