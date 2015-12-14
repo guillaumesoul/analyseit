5e7c1456aeebe697cd813c365662a1ac3fa87d7e
Guillaume Soullard <gsoullard@interactiv-technologies.com>
2015-12-15 00:18:38 +0100

    init calculated value

--- web/js/analyse.js
+++ web/js/analyse.js
@@ -19,11 +19,7 @@ $(document).ready(function() {
         dataTable.row($(this).closest('tr')).remove().draw( false );
     } );
 
-    /*myFather.name = function () {
-        return this.firstName + " " + this.lastName;
-    };*/
-
-    dataTable.getRawData = function(){
+    dataTable.getInputData = function(){
         //get the data from datatable
         var dtData = dataTable.data();
         var datatable_inputs = dataTable.$('input');
@@ -51,7 +47,6 @@ $(document).ready(function() {
                 temp2Associative = tempAssociative;
                 temp2 = temp;
                 seriesTab[cpt] = temp2;
-                //console.log(seriesTab);
                 seriesTabAssociative[cpt] = temp2Associative;
                 cpt++;
                 temp = [];
@@ -74,21 +69,47 @@ $(document).ready(function() {
         return response;
     }
 
+    dataTable.getParamsAttribute = function(){
+        var param = {};
+        var serie = [];
+        var paramsCells =  $('#param_datatable thead tr td div');
+        if ( paramsCells.length > 0) {
+            paramsCells.each(function() {
+                var paramsAttr = $(this).find('.paramAttribute');
+                if (paramsAttr.length > 0){
+                    paramsAttr.each(function(){
+                        param[$(this).attr('index')] = $(this).val();
+                    });
+                    serie.push(param);
+                    param = {};
+                }
+            });
+        }
+        return serie;
+    }
+
 
 
     $('#chart_button').on('click', function(){
 
-        var rawDatatableData = dataTable.getRawData();
+        var rawDatatableData = dataTable.getInputData();
+        var paramsAttr = dataTable.getParamsAttribute();
+        var calcChartValues = getChartCalcValues(paramsAttr, rawDatatableData.seriesDataAssociative);
+
 
         if ( $.fn.dataTable.isDataTable( '#example' ) ) {
             $('#example').DataTable().destroy();
         }
+
+        console.log(calcChartValues);
+        console.log(rawDatatableData.seriesData);
         var workTab = $('#example').DataTable( {
             "searching": false,
             "info" : false,
             "bLengthChange": false,
             "paging" : false,
-            data: rawDatatableData.seriesData,
+            //data: rawDatatableData.seriesData,
+            data: calcChartValues,
             columns: rawDatatableData.dtColumns
         } );
 
@@ -128,52 +149,77 @@ $(document).ready(function() {
 
 });
 
-/*
-function getDatatableRawData(dataTable)
+function getParamAttribute(paramId,paramsAttr)
 {
-    //get the data from datatable
-    var dtData = dataTable.data();
-    var datatable_inputs = dataTable.$('input');
-    var inputNbrByRow = dataTable.$('input').length / (dtData.length);
-    //cut your inputs into row
-
-    var indexTab = [];
-    var seriesTab = [];
-    var seriesTabAssociative = [];
-    var temp = [];
-    var tempAssociative = {};
-    var temp2 = [];
-    var temp2Associative = {};
-    var cpt = 0;
-    var relativeIndex = 0;
-
-    for (var i=0 ; i<datatable_inputs.length ; i++){
-        relativeIndex = i - cpt*inputNbrByRow;
-        tempAssociative[$(datatable_inputs[i]).attr('index')] = $(datatable_inputs[i]).val();
-        temp[relativeIndex] = $(datatable_inputs[i]).val();
-        //console.log(temp);
-        if (i<inputNbrByRow) {
-            indexTab[i] = $(datatable_inputs[i]).attr('index');
-        }
-        if (i%inputNbrByRow == inputNbrByRow-1) {
-            temp2Associative = tempAssociative;
-            temp2 = temp;
-            seriesTab[cpt] = temp2;
-            //console.log(seriesTab);
-            seriesTabAssociative[cpt] = temp2Associative;
-            cpt++;
-            temp = [];
-            tempAssociative = {};
-        }
+    var response = 'response';
+    for (var i=0;  i<paramsAttr.length ; i++){
+        var paramId = paramsAttr[i].paramId;
     }
+    return response;
+}
 
-    //console.log(indexTab);
-    //console.log(seriesTab);
-    //console.log(seriesTabAssociative);
+function transformParamAttrArray(paramsAttr)
+{
+    var response = [];
+    for (var i=0;  i<paramsAttr.length ; i++){
+        var paramId = paramsAttr[i].paramId;
+        response[paramId] = paramsAttr[i];
+    }
+    return response;
+}
+
+function calcValueWithMinMax(value, paramAttr)
+{
+    var response = 0;
+    var minValue = paramAttr.paramMinValue;
+    var maxValue = paramAttr.paramMaxValue;
+    if (minValue>maxValue){
+        var tmp = maxValue;
+        maxValue = minValue;
+        minValue = tmp;
+    }
+    if (!isNaN(value) && !isNaN(minValue) && !isNaN(maxValue)){
+        response = ((value) / (maxValue - minValue));
+    }
+    return response;
+}
+
+function getChartCalcValues(paramsAttr, rawDatatableData)
+{
+    //transform ton paramsAttr array avec paramId qui fait l'index
+    var paramsAttrByKeyId = transformParamAttrArray(paramsAttr);
 
     var response = {};
-    response.index = indexTab;
-    response.seriesData = seriesTab;
-    response.seriesDataAssociative = seriesTabAssociative;
-    return response;
-}*/
+    for (var i=0;  i<paramsAttr.length ; i++){
+        var paramId = paramsAttr[i].paramId
+    };
+
+    var tab = [];
+    var tmpTab = [];
+
+    var serie = {};
+    for (var i=0 ; i<rawDatatableData.length ; i++){
+        serie = {};
+        serie = rawDatatableData[i];
+        var serieIndex = Object.keys(serie);
+        for(var j=0 ; j<serieIndex.length ; j++){
+            var key = serieIndex[j];
+            var regex = /param_/;
+            var isParam = regex.test(key);
+            var value = serie[Object.keys(serie)[j]];
+            tmpTab[j] = 0;
+            if (isParam){
+                var paramId = key.replace(regex, "");
+                var paramAttr = paramsAttrByKeyId[paramId];
+                var calcValue = calcValueWithMinMax(value, paramAttr);
+                //rawDatatableData[i][Object.keys(serie)[j]] = calcValue;
+                tmpTab[j] = calcValue;
+            }else{
+                //rawDatatableData[i][Object.keys(serie)[j]] = value;
+                tmpTab[j] = value;
+            }
+        }
+        tab.push(tmpTab);
+    }
+    return tab;
+}
